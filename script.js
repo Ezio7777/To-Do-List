e3d27f49b9ff0cfd1aa7aeef4231c15f36c50ae8
sunit pal <sunitpal2000@gmail.com>
2023-07-09 00:32:51 +0530

    skeliton Done

---
+++ script.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+let items = [];
+
+app.get("/", function (req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { today: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Port Started");
+});
