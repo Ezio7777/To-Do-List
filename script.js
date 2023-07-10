b7441d03ed986581d7dcacfe703c07ab72caf86f
sunit pal <sunitpal2000@gmail.com>
2023-07-10 12:56:28 +0530

    adding own modules

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/own_Modules/date.js");
 
 const app = express();
 
@@ -12,15 +13,7 @@ app.set("view engine", "ejs");
 let items = [];
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDay();
   res.render("list", { today: day, newListItems: items });
 });
 
