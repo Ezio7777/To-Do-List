6c7cd19b4fad4f91a7aa229a74208218ab84deb8
sunit pal <sunitpal2000@gmail.com>
2023-07-16 22:52:13 +0530

    connect MongoDB

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/own_Modules/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,11 +11,39 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-let items = [];
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todolist")
+  .then(() => {
+    console.log("Succesfully Connected to the Mongodb Database");
+  })
+  .catch(() => {
+    console.log("Error Connecting to the Mongodb Database");
+  });
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Item name was EMPTY"],
+  },
+});
+
+const Item = mongoose.model("items", itemSchema);
+
+const intro = new Item({
+  name: "Add new Items by clicking + button",
+});
 
 app.get("/", function (req, res) {
   let day = date.getDay();
-  res.render("list", { today: day, newListItems: items });
+
+  Item.find().then((foundItem) => {
+    if (foundItem.length === 0) {
+      Item.insertMany(intro);
+      res.redirect("/");
+    } else {
+      res.render("list", { today: day, newListItems: foundItem });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
